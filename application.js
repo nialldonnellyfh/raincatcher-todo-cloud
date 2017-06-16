01073509d4aa42216a855a825abf38b513ee8678
Niall Donnelly <niall.donnelly@feedhenry.com>
2017-06-16 12:28:07 +0100

    Added workorder, sync and storage raincatcher modules

--- application.js
+++ application.js
@@ -3,9 +3,10 @@ var express = require('express');
 var mbaasExpress = mbaasApi.mbaasExpress();
 var cors = require('cors');
 
+
 // list the endpoints which you want to make securable here
 var securableEndpoints;
-securableEndpoints = ['/hello'];
+securableEndpoints = [];
 
 var app = express();
 
@@ -16,34 +17,29 @@ app.use(cors());
 app.use('/sys', mbaasExpress.sys(securableEndpoints));
 app.use('/mbaas', mbaasExpress.mbaas);
 
-/* uncomment this code if you want to use $fh.auth in the app preview
- * localAuth is only used for local development. 
- * If the app is deployed on the platform, 
- * this function will be ignored and the request will be forwarded 
- * to the platform to perform authentication.
-
-app.use('/box', mbaasExpress.auth({localAuth: function(req, cb){
-  return cb(null, {status:401, body: {"message": "bad request"}});
-}}));
-
-or
-
-app.use('/box', mbaasExpress.core({localAuth: {status:401, body: {"message": "not authorised”}}}));
-*/
-
 // allow serving of static files from the public directory
 app.use(express.static(__dirname + '/public'));
 
+//Setting up the raincatcher workorders
+
+
+
 // Note: important that this is added just before your own Routes
 app.use(mbaasExpress.fhmiddleware());
-
-app.use('/hello', require('./lib/hello.js')());
 
 // Important that this is last!
 app.use(mbaasExpress.errorHandler());
 
 var port = process.env.FH_PORT || process.env.OPENSHIFT_NODEJS_PORT || 8001;
 var host = process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
-app.listen(port, host, function() {
-  console.log("App started at: " + new Date() + " on port: " + port); 
+
+mbaasApi.events.on('sync:ready', function() {
+  require('./lib/setUpRaincatcher')().then(function() {
+    app.listen(port, host, function() {
+      console.log("App started at: " + new Date() + " on port: " + port);
+    });
+  });
 });
+
+
+
